1e7cd81d97fc024781cd26c687c54dd13a35d8aa
Luciano Borges <lucianosantosborges@gmail.com>
2019-09-04 22:43:38 +0100

    Criação do projeto, iboutets, ibactions e navegação

---
+++ Mensagens/BaseViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    @IBOutlet weak var lbMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func changeColor(_ sender: UIButton) {
+        
+    }
+    
+}
